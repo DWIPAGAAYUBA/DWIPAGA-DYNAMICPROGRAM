ea07721dc9d304b4cf712fcc62f84c88eafb862b
MSI <MSI@DESKTOP-DRHGVJJ>
2018-09-14 21:08:51 +0700

    Initial commit

---
+++ src/Dynamicprogram/dynamic.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Dynamicprogram;
+import java.math.BigInteger;
+import java.util.Scanner;
+/**
+ *
+ * @author MSI
+ */
+public class dynamic {
+     public static void tampilJudul(String identitas) {
+        System.out.println("Identitas : "+ identitas);
+        
+        System.out.println("\nHitung Fibonacci");
+        System.out.println("1, 2, 3, 5, 8, 13, 21, ...dst.\n");
+    }
+    public static void main(String[] args){
+        String identitas = "Dwi paga ayuba / X RPL 2 / 10";
+        
+        tampilJudul(identitas);
+}
