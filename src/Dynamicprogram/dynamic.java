f7969ce3ddd09ada8afaa37e7c3060c4f74bbf2d
MSI <MSI@DESKTOP-DRHGVJJ>
2018-09-14 21:12:48 +0700

    Tambah Hitung Fibonacci

--- src/Dynamicprogram/dynamic.java
+++ src/Dynamicprogram/dynamic.java
@@ -33,4 +33,17 @@ public class dynamic {
         int n = scanner.nextInt();
         
         return n;
+          }
+  
+    private static BigInteger fibo(int n){
+        
+        BigInteger[] hasil = new BigInteger[n];
+        
+        hasil[0] = BigInteger.ONE;
+        hasil[1] = BigInteger.ONE;
+        
+        for (int i = 2; i < n; i++){
+            hasil[i] = hasil[i-1].add(hasil[i-2]);
+        }
+        return hasil[n-1];
 }
