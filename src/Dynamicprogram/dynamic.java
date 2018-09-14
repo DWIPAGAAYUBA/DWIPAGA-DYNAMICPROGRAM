cbc8d4dfe0432c97f6f9ce0cd778ec3735331a4d
MSI <MSI@DESKTOP-DRHGVJJ>
2018-09-14 21:13:42 +0700

    Tambah Tampil Hasil

--- src/Dynamicprogram/dynamic.java
+++ src/Dynamicprogram/dynamic.java
@@ -46,4 +46,9 @@ public class dynamic {
             hasil[i] = hasil[i-1].add(hasil[i-2]);
         }
         return hasil[n-1];
+        
+         }
+    private static void tampilHasil(int n, BigInteger hasil){
+        System.out.println("Bilangan Fibonacci ke-"+n+" : " + hasil);
+    }
 }
